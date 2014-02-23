638df65f12415306b6c1c5c26896e6794c42c4fe
yadbatya <yadid89@gmail.com>
2014-02-23 19:34:32 +0200

    Create request.js

---
+++ request.js
@@ -0,0 +1,29 @@
+module.exports = function(req, cb) {
+    var utils = require('./utils');
+    req.cookies = req.headers.Cookie || {};
+    req.params = {};
+    if (req.url.indexOf('?') !== -1) {
+        var qs = req.url.substring(req.url.indexOf('?') + 1);
+    }
+    req.query = {};
+    if (qs) {
+        utils.miniQueryString(qs, req.query);
+    }
+    req.protocol = 'http';
+    req.host = req.headers['host'];
+    req.path = req.url.substring(0 , req.url.indexOf('?')) || req.url;
+
+    req.is = function(type) {
+        var regEx = new RegExp(type);
+        return regEx.test(req.headers['content-type']);
+    }
+
+    req.param = function(name) {
+        return req.param[name] || req.body[name] || req.query[name];
+    }
+
+    req.get = function(field) {
+        return req.headers[field.toLowerCase()];
+    }
+    cb();
+}
